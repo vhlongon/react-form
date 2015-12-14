05ce812f3df1012aa688e8619011b8440d6c514a
vhlongon <victor.longon@zooma.se>
2015-12-14 22:16:20 +0100

    gulp working properly after clean npm cache

---
+++ src/includes/CommentForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="Commentform">
+        Hello, world! I am a comment form!
+      </div>
+    );
+  }
+}
--- src/includes/Commentbox.js
+++ src/includes/Commentbox.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
 
 export default class Commentbox extends React.Component {
   render() {
     return (
       <div className="commentbox">
-        Hello, world! I am a comment box!
+      	<h1>Comments</h1>
+      	<CommentList/>
+      	<CommentForm/>
       </div>
     );
   }
