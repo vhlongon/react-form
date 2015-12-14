66b38d2f69aea6d572551ad6ca64091f41085a86
vhlongon <victor.longon@zooma.se>
2015-12-14 11:52:14 +0100

    remove teste files originally from boilerplate

--- src/app.js
+++ src/app.js
@@ -2,19 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactComp from './includes/ReactComp';
-import $ from 'jquery';
-import {Person} from './includes/Person';
-import * as test from './includes/test';
-
-//also possible to use require syntax
-require('browsernizr/test/fullscreen-api');
-require('browsernizr/test/css/columns');
-let Modernizr = require('browsernizr');
 
 ReactDOM.render(<ReactComp name="Lord Vader"/>,  document.querySelector('.react-root')); 
-$('body').css('background', 'red');
 global.app = function main() {
-  let person = new Person('Jerry Lee', 'Lewis');
-  console.log(person.fullName);
-  console.log(Modernizr);
+
 };
--- src/includes/test.js
+++
@@ -1,4 +0,0 @@
-import {anotherTest} from './anotherTest';
-let [a, b] = [1, 2];
-console.log(`testing some string interpolation a = ${a} and b = ${b}`);
-anotherTest();
