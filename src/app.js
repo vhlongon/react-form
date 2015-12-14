c076c868a5e45eca4d48a6f0f538e1e3474251ab
vhlongon <victor.longon@zooma.se>
2015-12-14 20:00:51 +0100

    clean dependecies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // Import modules using ES2015
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactComp from './includes/ReactComp';
+import CommentBox from './includes/Commentbox';
 
-ReactDOM.render(<ReactComp name="Lord Vader"/>,  document.querySelector('.react-root')); 
-global.app = function main() {
+window.React = require('React');
 
-};
+ReactDOM.render(<CommentBox/>,  document.querySelector('.react-root')); 
+
---
+++ src/includes/Commentbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Commentbox extends React.Component {
+  render() {
+    return (
+      <div className="commentbox">
+        Hello, world! I am a comment box!
+      </div>
+    );
+  }
+}
+
